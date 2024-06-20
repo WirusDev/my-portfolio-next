6605144898a6db32e0b546dd23fe9d3a98205e08
WirusDev <zlarin63@gmail.com>
2024-06-20 23:05:30 +0200

    init next

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-blue-500 p-4 text-white">
+      <h1>My Portfolio</h1>
+    </header>
+  );
+};
+
+export default Header;
