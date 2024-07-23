d5fb6898529c590ad1a2890ef4ac093d2e22759c
WirusDev <zlarin63@gmail.com>
2024-07-23 12:03:42 +0200

    Footer UI fix

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import Link from "next/link";
 const Footer: React.FC = () => {
   return (
-    <footer className='bg-gray-800 p-4 text-white mt-4 flex justify-between'>
-      <p>&copy; 2024 Portfolio by Evgenii Larin. </p>
-      <p>Alle Rechte vorbehalten.</p>
-      <div>
-        <Link href='/legal/impressum' className='mr-4'>
-          Impressum
-        </Link>
-        <Link href='/legal/datenschutz'>Datenschutz</Link>
-      </div>
-    </footer>
+    <>
+      {/* OLD */}
+      <footer className='bg-gray-800 p-4 text-white mt-4 flex md:flex-row flex-col justify-between'>
+        <p>&copy; {new Date().getFullYear()} Portfolio by Evgenii Larin </p>
+        {/* <p>Alle Rechte vorbehalten.</p> */}
+        <div>
+          <Link href='/legal/impressum' className='mr-4'>
+            Impressum
+          </Link>
+          <Link href='/legal/datenschutz'>Datenschutz</Link>
+        </div>
+      </footer>
+    </>
   );
 };
 
