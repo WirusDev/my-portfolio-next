4342df6f8e357742abc86048213b4ce4bda12762
WirusDev <zlarin63@gmail.com>
2024-07-23 12:54:21 +0200

    About - info update

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -54,6 +54,43 @@ export default function About() {
             <strong>Projektmanagement:</strong> Ich habe Erfahrung im Management
             von Projekten, von der Planung bis zur Implementierung und Wartung.
           </li>
+          <li>
+            <strong>Heimserver und Systemadministration:</strong> Als Hobby habe
+            ich einen Heimserver mit TrueNAS Scale aufgebaut. Ich habe die
+            gesamte Konfiguration und Wartung des Servers übernommen,
+            einschließlich:
+            <ul>
+              <li>
+                <strong>Systemkonfiguration:</strong> Hardware- und
+                Softwareeinstellungen für optimale Leistung.
+              </li>
+              <li>
+                <strong>Datenverwaltung:</strong> Implementierung von
+                Speicherpools und effiziente Datenorganisation.
+              </li>
+              <li>
+                <strong>Netzwerkverwaltung:</strong> Konfiguration von
+                Netzwerkschnittstellen und sicheren Fernzugriff.
+              </li>
+              <li>
+                <strong>Virtualisierung:</strong> Erstellung und Verwaltung von
+                virtuellen Maschinen und Containern (z.B. mit Docker).
+              </li>
+              <li>
+                <strong>Backup und Wiederherstellung:</strong> Einrichtung
+                automatisierter Backup-Lösungen und Erlernen von
+                Wiederherstellungsverfahren.
+              </li>
+              <li>
+                <strong>Sicherheit:</strong> Implementierung von
+                Sicherheitsmaßnahmen wie Firewalls, VPNs und SSH-Schlüsseln.
+              </li>
+              <li>
+                <strong>Automatisierung: </strong>Nutzung von Skripten und
+                Cron-Jobs zur Automatisierung von Routineaufgaben.
+              </li>
+            </ul>
+          </li>
         </ul>
         <br />
         <h2 className='text-2xl'>Persönliche Eigenschaften</h2>
